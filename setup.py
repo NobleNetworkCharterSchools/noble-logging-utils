639be7ad739b84ac667cd30afa4d55cf4ec3fe0a
Joe Ravesloot <jravesloot@noblenetwork.org>
2017-10-19 14:12:21 -0500

    Add setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+# noble_logging_utils/setup.py
+
+from distutils.core import setup
+
+setup(
+    name="noble_logging_utils",
+    version="0.1",
+    packages=[],
+    #scripts=["bin/emulambda"],
+    url="https://www.github.com/noblenetworkcharterschools/noble-logging-utils",
+    author="Noble Network of Charter Schools",
+    description="Logging utilities",
+    install_requires=[],
+)
